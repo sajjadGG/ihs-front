cfc192eb2fa68d663e44a104cfa1f338d7f31fa6
unknown <m.eslampanah29@gmail.com>
2020-04-27 16:19:19 +0430

    Add home & profile to navdropdown

--- src/PatientPages/AppointmentHistory.js
+++ src/PatientPages/AppointmentHistory.js
@@ -5,45 +5,51 @@ import App from '../App';
 
 function AppointmentHistory(props){
 
+    const [Num , setNum] = useState(props.Num ? props.Num : null);
+    const [Docter , setDocter] = useState(props.Docter ? props.Docter : null);
+    const [Proficiency , setProficiency] = useState(props.Proficiency ? props.Proficiency : null);
+    const [AppDate , setAppDate] = useState(props.AppDate ? props.AppDate : null);
+    const [AppState , setAppstate] = useState(props.AppState ? props.AppState : null);
+
+
     let color=null;
-    const AppState = 0;
-    if(AppState == 0){
+
+    if(AppState == "نا موفق"){
         color="rgb(202, 33, 33)";
     }
-    else if (AppState == 1 ) {
+    else if (AppState == "رزرو شده") {
         color="rgb(38, 226, 38)";
     }
-    else if(AppState ==2){
+    else if(AppState == "انجام شده"){
         color="rgb(64, 64, 255)";
+    }
+    else{
+        color="lightgray"
     }
    
     const Appointment =
-    <div className="table container">
-        <div className="Thead row">
-            <div className="Thid col-md-1"><h3>شماره نوبت</h3></div>
-            <div className="Thid col-md-3"><h3>دکتر</h3></div>
-            <div className="Thid col-md-3"><h3>تخصص</h3></div>
-            <div className="Thid col-md-3"><h3>تاریخ</h3></div>
-            <div className="Thid col-md-2"><h3>وضعیت</h3></div>
-        </div>   
-        <div className="Tt row" style={{backgroundColor: color}}>
-            <div className="Tbid col-md-1"><h3>1</h3></div>
-            <div className="Tbid col-md-3"><h3>علی محمدی</h3></div>
-            <div className="Tbid col-md-3"><h3>اعصاب</h3></div>
-            <div className="Tbid col-md-3"><h3>98/12/12</h3></div>
-            <div className="Tbid col-md-2"><h3>انجام شده</h3></div>
-        </div>   
-    </div>
-    
+    <div className="Tt row" style={{backgroundColor: color}}>
+    <div className="Tbid col-md-1"><h3>{Num}</h3></div>
+    <div className="Tbid col-md-3"><h3>{Docter}</h3></div>
+    <div className="Tbid col-md-3"><h3>{Proficiency}</h3></div>
+    <div className="Tbid col-md-3"><h3>{AppDate}</h3></div>
+    <div className="Tbid col-md-2"><h3>{AppState}</h3></div>
+</div> 
+
         return(
             <div >
                 <hr style= {{marginTop : "40px"}}></hr>
-                <div className="container ">
-
-                {Appointment}
-
-            <h1 > "HHHHHH"</h1>
-            </div></div>
+                <div className="table container">
+                    <div className="Thead row">
+                        <div className="Thid col-md-1"><h3>شماره نوبت</h3></div>
+                        <div className="Thid col-md-3"><h3>دکتر</h3></div>
+                        <div className="Thid col-md-3"><h3>تخصص</h3></div>
+                        <div className="Thid col-md-3"><h3>تاریخ</h3></div>
+                        <div className="Thid col-md-2"><h3>وضعیت</h3></div>
+                    </div>   
+                    {Appointment}
+                </div>
+            </div>
         );
 
     
