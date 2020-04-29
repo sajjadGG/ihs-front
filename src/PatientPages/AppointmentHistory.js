b6e850b05b1195cbe86454f1377644a004521de2
unknown <m.eslampanah29@gmail.com>
2020-04-30 02:26:03 +0430

    edit & del row in AppointmentHistory

--- src/PatientPages/AppointmentHistory.js
+++ src/PatientPages/AppointmentHistory.js
@@ -2,7 +2,10 @@ import React ,{ useState } from 'react';
 
 function AppointmentHistory(props){
     const [records , setRecords] = useState(props.records ? props.records : null)
-    
+    const [EditC , setEditC] = useState(null)
+    let Del = (Num) => {
+        setRecords(records.filter(i => i.Num != Num))
+    }
 
     return(
         <div >
@@ -16,16 +19,24 @@ function AppointmentHistory(props){
                         <div className="Thid col-md-2"><h3>وضعیت</h3></div>
                     </div>   
                 {
+                    
                 records && records.map(element => {
                     let color;
+                    let Cancel = null;
+                    let Edit = null;
+                    let laqv =<div onClick={()=> Del(element.Num)} style={{cursor:"pointer",backgroundColor:"rgb(114, 114, 114)" , border:"1px solid blue" , borderRadius:"10px"}}><h2>لغو</h2></div>
+
+          
                     if(element.AppState == "نا موفق"){
-                        color="rgb(202, 33, 33)";
+                        color="rgb(255, 129, 129)";
                     }
                     else if (element.AppState == "رزرو شده") {
-                        color="rgb(38, 226, 38)";
+                        color="rgb(140, 228, 140)";
+                        Edit = <i className="far fa-edit" onClick={EditC?()=>setEditC(null):()=>setEditC(laqv)} style={{cursor:"pointer"}}></i>
+                        Cancel = EditC;
                     }
                     else if(element.AppState == "انجام شده"){
-                        color="rgb(64, 64, 255)";
+                        color="rgb(135, 135, 243)";
                     }
                     else{
                         color="lightgray"
@@ -36,7 +47,7 @@ function AppointmentHistory(props){
                     <div className="Tbid col-md-3"><h3>{element.Docter}</h3></div>
                     <div className="Tbid col-md-3"><h3>{element.Proficiency}</h3></div>
                     <div className="Tbid col-md-3"><h3>{element.AppDate}</h3></div>
-                    <div className="Tbid col-md-2"><h3>{element.AppState}</h3></div>
+                    <div className="Tbid col-md-2"><h3>{element.AppState}{Edit}{Cancel}</h3></div>
                             </div>
                     )
                 })}
